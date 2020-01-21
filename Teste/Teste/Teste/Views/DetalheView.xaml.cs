f5bb7b69069da33f7812b141ffd720a7f6e95161
heron03 <heronze@gmail.com>
2020-01-21 17:02:44 -0300

    Refatorando o Detalhe

--- Teste/Teste/Teste/Views/DetalheView.xaml.cs
+++ Teste/Teste/Teste/Views/DetalheView.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Teste.Models;
+using Teste.ViewModels;
 using Xamarin.Forms;
 
 namespace Teste.Views
@@ -12,83 +13,11 @@ namespace Teste.Views
     {
         public Veiculo Veiculo { get; set; }
 
-        public string TextoFreioABS
-        {
-            get
-            {
-                return string.Format("Freio ABS - R$ {0}", Veiculo.FREIO_ABS);
-            }
-        }
-
-        public string TextoMP3
-        {
-            get
-            {
-                return string.Format("Radio MP3 - R$ {0}", Veiculo.MP3_PLAYER);
-            }
-        }
-
-        public string TextoAr
-        {
-            get
-            {
-                return string.Format("Ar - R$ {0}", Veiculo.AR_CONDICIONADO);
-            }
-        }
-
-        public string ValorTotal
-        {
-            get
-            {
-                return Veiculo.PrecoTotalFormatado;
-            }
-        }
-
-        public bool TemFreioABS
-        {
-            get
-            {
-                return Veiculo.temFreioABS;
-            }
-            set
-            {
-                Veiculo.temFreioABS = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            }
-        }
-
-        public bool TemMP3 { 
-            get
-            {
-                return Veiculo.temMP3;
-            }
-            set
-            {
-                Veiculo.temMP3 = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            }
-        }
-
-        public bool TemArCondicionado {
-             get
-            {
-                return Veiculo.temArCondicionado;
-            }
-            set
-            {
-                Veiculo.temArCondicionado = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            }
-        }
-
         public DetalheView(Veiculo veiculo)
         {
-            this.Veiculo = veiculo;
             InitializeComponent();
-            this.BindingContext = this;
+            this.Veiculo = veiculo;
+            this.BindingContext = new DetalheViewModel(veiculo);
         }
 
         private void botaoProximo_Clicked(object sender, EventArgs e)
