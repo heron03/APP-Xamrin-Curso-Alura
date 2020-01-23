ce861e109a2cc78079493dbb9f1812fdc1db6a50
heron03 <heronze@gmail.com>
2020-01-23 09:36:35 -0300

    Video 8

---
+++ Teste/Teste/Teste/ViewModels/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Teste.Models;
+using Xamarin.Forms;
+
+namespace Teste.ViewModels
+{
+    public class LoginViewModel
+    {
+        private string usuario;
+        public string Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        private string senha;
+        public string Senha
+        {
+            get { return senha; }
+            set { senha = value; }
+        }
+        public ICommand EntrarCommand { get; private set; }
+
+        public LoginViewModel()
+        {
+            EntrarCommand = new Command(() =>
+            {
+                MessagingCenter.Send<Usuario>(new Usuario(), "SucessoLogin");
+            });
+        }
+    }
+}
