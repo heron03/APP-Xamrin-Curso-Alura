e5dba7b0ec82476da67d74f13f255b56efb29ad3
heron03 <heronze@gmail.com>
2020-01-23 10:07:31 -0300

    Video 10

--- Teste/Teste/Teste/ViewModels/LoginViewModel.cs
+++ Teste/Teste/Teste/ViewModels/LoginViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Teste.Models;
 using Xamarin.Forms;
@@ -13,23 +15,52 @@ namespace Teste.ViewModels
         public string Usuario
         {
             get { return usuario; }
-            set { usuario = value; }
+            set
+            {
+                usuario = value;
+                ((Command)EntrarCommand).ChangeCanExecute();
+            }
         }
 
         private string senha;
         public string Senha
         {
             get { return senha; }
-            set { senha = value; }
+            set
+            {
+                senha = value;
+                ((Command)EntrarCommand).ChangeCanExecute();
+            }
         }
+
         public ICommand EntrarCommand { get; private set; }
 
         public LoginViewModel()
         {
-            EntrarCommand = new Command(() =>
+            EntrarCommand = new Command(async () =>
             {
-                MessagingCenter.Send<Usuario>(new Usuario(), "SucessoLogin");
+                await FazerLogin();
+            }, () =>
+            {
+                return !string.IsNullOrEmpty(usuario)
+                        && !string.IsNullOrEmpty(senha);
             });
+        }
+
+        private static async Task FazerLogin()
+        {
+            using (var cliente = new HttpClient())
+            {
+                var camposFormulario = new FormUrlEncodedContent(new[]
+                  {
+                        new KeyValuePair<string, string>("email", "joao@alura.com.br"),
+                        new KeyValuePair<string, string>("senha", "alura123")
+                      });
+
+                cliente.BaseAddress = new Uri("https://aluracar.herokuapp.com");
+                await cliente.PostAsync("/login", camposFormulario);
+                MessagingCenter.Send<Usuario>(new Usuario(), "SucessoLogin");
+            }
         }
     }
 }
