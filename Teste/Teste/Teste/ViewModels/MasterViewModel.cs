e1611c3d2a2b75c7e2b1bfe4b80faea907200785
heron03 <heronze@gmail.com>
2020-01-24 08:01:35 -0300

    video 10

--- Teste/Teste/Teste/ViewModels/MasterViewModel.cs
+++ Teste/Teste/Teste/ViewModels/MasterViewModel.cs
@@ -1,30 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 using Teste.Models;
+using Xamarin.Forms;
 
 namespace Teste.ViewModels
 {
-    public class MasterViewModel
-    {
+    public class MasterViewModel : BaseViewModel
+	{
+        public string Nome
+        {
+            get
+            {
+                return this.usuario.nome;
+            }
+            set
+            {
+                this.usuario.nome = value;
+            }
+        }
 
-		public string Nome
-		{
-			get { return this.usuario.nome; }
-			set { this.usuario.nome = value; }
-		}
+        public string Email
+        {
+            get { return this.usuario.email; }
+            set
+            {
+                this.usuario.email = value;
+            }
+        }
 
-		public string Email
-		{
-			get { return this.usuario.email; }
-			set { this.usuario.email = value; }
-		}
+        public string DataNascimento
+        {
+            get { return usuario.dataNascimento; }
+            set { usuario.dataNascimento = value; }
+        }
 
-		private readonly Usuario usuario;
+        public string Telefone
+        {
+            get { return usuario.telefone; }
+            set { usuario.telefone = value; }
+        }
 
-		public MasterViewModel(Usuario usuario)
-		{
-			this.usuario = usuario;
-		}
-	}
+        private bool editando;
+        public bool Editando
+        {
+            get { return editando; }
+            private set
+            {
+                editando = value;
+                OnPropertyChanged(nameof(Editando));
+            }
+        }
+
+        private readonly Usuario usuario;
+
+        public ICommand EditarPerfilCommand { get; private set; }
+        public ICommand EditarCommand { get; private set; }
+        public ICommand SalvarCommand { get; private set; }
+
+        public MasterViewModel(Usuario usuario)
+        {
+            this.usuario = usuario;
+
+            EditarPerfilCommand = new Command(() =>
+            {
+                MessagingCenter.Send<Usuario>(usuario, "EditarPerfil");
+            });
+
+            EditarCommand = new Command(() =>
+            {
+                Editando = true;
+            });
+
+            SalvarCommand = new Command(() =>
+            {
+                Editando = false;
+                MessagingCenter.Send<Usuario>(usuario, "SucessoSalvarUsuario");
+            });
+        }
+    }
 }
--- Teste/Teste/Teste/Views/MasterView.xaml.cs
+++ Teste/Teste/Teste/Views/MasterView.xaml.cs
@@ -19,5 +19,37 @@ namespace Teste.Views
         }
 
         public MasterViewModel ViewModel { get; set; }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            AssinarMensagens();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            CancelarMensagens();
+        }
+
+        private void AssinarMensagens()
+        {
+            MessagingCenter.Subscribe<Usuario>(this, "EditarPerfil",
+                            (usuario) =>
+                            {
+                                this.CurrentPage = this.Children[1];
+                            });
+            MessagingCenter.Subscribe<Usuario>(this, "SucessoSalvarUsuario", (usuario) =>
+            {
+                this.CurrentPage = this.Children[0];
+            });
+        }
+
+        private void CancelarMensagens()
+        {
+            MessagingCenter.Unsubscribe<Usuario>(this, "EditarPerfil");
+            MessagingCenter.Unsubscribe<Usuario>(this, "SucessoSalvarUsuario");
+        }
+
     }
 }
